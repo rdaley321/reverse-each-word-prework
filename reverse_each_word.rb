382a47069e1aff5918406d440f9ceb9a1d214dff
Ryan Daley <rdaley321@yahoo.com>
2018-11-01 03:14:38 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,4 @@
 def reverse_each_word(sentence)
   array = sentence.split
-  print array
   array.collect{|x| x.reverse}.join(" ")
-  print array
 end
-
-print reverse_each_word("Hello, how are you doing?")
